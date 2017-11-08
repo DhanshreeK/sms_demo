6a01a0192cbad0e0559b5679048cae3f9eebb344
DhanshreeK <dhanshree.j.kamble@gmail.com>
2017-11-08 11:37:39 +0530

    added html interagtion + employees + generalsettings + myprofile + change password +center

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
+  resources :employees
+  resources :centers
+  devise_for :users, controllers: { registrations: 'registrations' }
+  as :user do
+  get '/users/sign_out' => 'devise/sessions#destroy'
+end
+
+  root 'home#dashboard'
+  resources :general_settings
+  
+  resources :home do
+    collection do 
+    get :dashboard
+    end
+  end
+
+  resources :users do
+     member do
+      get :change_password
+      patch :update_password
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20171107041628_create_centers.rb
@@ -0,0 +1,19 @@
+class CreateCenters < ActiveRecord::Migration[5.1]
+  def change
+    create_table :centers do |t|
+      t.string :center_name
+      t.string :address
+      t.string :city
+      t.string :state
+      t.string :pan_card_no
+      t.string :contact
+      t.string :alternate_contact
+      t.date :center_starting_date
+      t.string :contact_person_name
+      t.string :residential_address
+      t.string :center_code
+
+      t.timestamps
+    end
+  end
+end
