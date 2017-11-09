a80a09820b302b6b7a63dd2688d6503b29744e71
DhanshreeK <dhanshree.j.kamble@gmail.com>
2017-11-09 18:55:59 +0530

    added secret key

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module EducationalConsultancy
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+    config.secret_key_base = 'f2f926f6d740f3439dd2ec0ae007bf1b0b81ed2b960bda2873822a10f07dd26eebd6abc2b861706bf7beecd8407171eeb77c138eac26ec8b7229dc073326a25d'
+
   end
 end
