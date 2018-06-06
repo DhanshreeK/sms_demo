32e3589e080b15f5f41f5aaa5b4c90f9a9ac223d
DhanshreeK <dhanshree.j.kamble@gmail.com>
2018-06-06 16:20:25 +0530

    added path

--- app/models/student_detail.rb
+++ app/models/student_detail.rb
@@ -5,7 +5,7 @@ class StudentDetail < ApplicationRecord
 	has_many :comments, dependent: :destroy
 	belongs_to :country
 	belongs_to :medical_college
-	has_many :selected_courses
+	has_many :selected_courses, dependent: :destroy
 	belongs_to :budget
 	has_many :courses , through: :selected_courses , dependent: :destroy
 
