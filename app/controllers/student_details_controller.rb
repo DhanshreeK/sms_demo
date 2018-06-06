81d4753d99f9c1f82706a49777c4a3ed7e67a19d
DhanshreeK <dhanshree.j.kamble@gmail.com>
2018-06-06 16:11:26 +0530

    added budget

--- app/controllers/student_details_controller.rb
+++ app/controllers/student_details_controller.rb
@@ -98,20 +98,13 @@ end
     end
     end
   end
-
-
-      
-
-        
-
-        
        
   # PATCH/PUT /student_details/1
   # PATCH/PUT /student_details/1.json
   def update
     respond_to do |format|
       if @student_detail.update(student_detail_params)
-        format.html { redirect_to @student_detail, notice: 'Student detail was successfully updated.' }
+        format.html { redirect_to student_details_path, notice: 'Student detail was successfully updated.' }
         format.json { render :show, status: :ok, location: @student_detail }
       else
         format.html { render :edit }
