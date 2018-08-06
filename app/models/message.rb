cc6ba59d957947410337aa065f192ac59c6a172f
DhanshreeK <dhanshree.j.kamble@gmail.com>
2018-08-06 12:58:55 +0530

    bulk msg completed

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,7 +6,8 @@ class Message < ApplicationRecord
 	def send_message
 	    require 'uri'
 	    require 'net/http'
-        url = URI("http://api.msg91.com/api/sendhttp.php?sender=MSGIND&route=4&mobiles=#{self.contact.contact_number}&authkey=229724AhpqDNq13U5b642d8e&country=91&message=#{self.body}")
+	    url = URI("http://api.msg91.com/api/sendhttp.php?sender=JAWAAN&route=4&mobiles=#{self.contact.contact_number}&authkey=230171A29wCbr1l5b67ee39&country=91&message=#{self.body}")
+        # url = URI("http://api.msg91.com/api/sendhttp.php?sender=MSGIND&route=4&mobiles=#{self.contact.contact_number}&authkey=230171A29wCbr1l5b67ee39&country=91&message=#{self.body}")
 	    http = Net::HTTP.new(url.host, url.port)
 	    request = Net::HTTP::Get.new(url)
 	    response = http.request(request)
