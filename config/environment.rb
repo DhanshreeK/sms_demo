b2093aea37afbe2a7430983b825b3df1f14f47a6
DhanshreeK <dhanshree.j.kamble@gmail.com>
2018-08-08 15:05:07 +0530

    changed env domain

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => 'apikey',
   :password => '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',
-  :domain => 'localhost:3000',
+  :domain => 'https://qsetsmsdemo.herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
