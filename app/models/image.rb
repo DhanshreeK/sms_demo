9bccba8f1946545d9b62393eb333ab955bdccfc9
DhanshreeK <dhanshree.j.kamble@gmail.com>
2018-08-14 13:25:18 +0530

    shrine and secrets

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,4 @@
 class Image < ApplicationRecord
 	# adds an `image` virtual attribute
 include ::PhotoUploader::Attachment.new(:image)
-    validates_attachment :image, matches: [/png\Z/, /jpeg\Z/, /tiff\Z/, /bmp\Z/, /jpg\Z/]
 end
--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -26,6 +26,7 @@ else
 
 end
 Shrine.plugin :activerecord # or :activerecord
-Shrine.plugin :cached_attachment_data # for retaining the cached file across form redisplays
-Shrine.plugin :restore_cached_data # re-extract metadata when attaching a cached file
-Shrine.plugin :rack_file # for non-Rails apps
+# Shrine.plugin :cached_attachment_data # for retaining the cached file across form redisplays
+# Shrine.plugin :restore_cached_data # re-extract metadata when attaching a cached file
+# Shrine.plugin :rack_file # for non-Rails apps
+# Shrine.plugin :endpoint_provider
